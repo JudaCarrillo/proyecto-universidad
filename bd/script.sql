859a217afec422931ffc3d15e1d607a6612d15f4
Jhosue <150406643+JhosueLeo@users.noreply.github.com>
2023-11-15 00:57:42 -0500

    Bd

--- bd/script.sql
+++ bd/script.sql
@@ -1,31 +1,33 @@
 -- Tabla Carrera
 CREATE TABLE Carrera (
-    idCarrera INT IDENTITY PRIMARY KEY ,
+    idCarrera INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL
 );
 
 -- Tabla Profesor
 CREATE TABLE Profesor (
-    idProfesor INT IDENTITY PRIMARY KEY ,
+    idProfesor INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-	correo VARCHAR(100) NOT NULL,
-	contraseña VARCHAR(100) NOT NULL
+    correo VARCHAR(100) NOT NULL,
+    contraseña VARCHAR(100) NOT NULL
 );
 
 -- Tabla Alumnos
 CREATE TABLE Alumnos (
-    idAlumno INT IDENTITY PRIMARY KEY,
+    idAlumno INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-	correo VARCHAR(100) NOT NULL,
-	contraseña VARCHAR(100) NOT NULL
+    correo VARCHAR(100) NOT NULL,
+    contraseña VARCHAR(100) NOT NULL
 );
 
 -- Tabla Materia
 CREATE TABLE Materia (
-    idMateria INT IDENTITY PRIMARY KEY,
+    idMateria INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     idCarrera INT,
-    idProfesor INT
+    idProfesor INT,
+    FOREIGN KEY (idCarrera) REFERENCES Carrera(idCarrera),
+    FOREIGN KEY (idProfesor) REFERENCES Profesor(idProfesor)
 );
 
 -- Tabla Alumnos_Materias (Tabla intermedia)
@@ -39,29 +41,28 @@ CREATE TABLE Alumnos_Materias (
 
 -- Tabla Horario
 CREATE TABLE Horario (
-    idHorario INT IDENTITY PRIMARY KEY ,
-	idMateria INT,
+    idHorario INT AUTO_INCREMENT PRIMARY KEY,
+    idMateria INT,
     horaInicio TIME NOT NULL,
-    horaFin TIME NOT NULL
-	FOREIGN KEY (idMateria) REFERENCES Materia(idMateria)
+    horaFin TIME NOT NULL,
+    FOREIGN KEY (idMateria) REFERENCES Materia(idMateria)
 );
 
 -- Tabla Aulas
 CREATE TABLE Aulas (
-    idAula INT IDENTITY PRIMARY KEY ,
+    idAula INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL
 );
 
 -- Tabla Laboratorios
 CREATE TABLE Laboratorios (
-    idLaboratorio INT IDENTITY PRIMARY KEY ,
+    idLaboratorio INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL
-	
 );
 
 -- Tabla Asistencia
 CREATE TABLE Asistencia (
-    idAsistencia INT IDENTITY PRIMARY KEY,
+    idAsistencia INT AUTO_INCREMENT PRIMARY KEY,
     idAlumno INT,
     idMateria INT,
     FOREIGN KEY (idAlumno) REFERENCES Alumnos(idAlumno),
@@ -70,29 +71,29 @@ CREATE TABLE Asistencia (
 
 -- Tabla CronogramaPagos
 CREATE TABLE CronogramaPagos (
-    idCronogramaPagos INT IDENTITY PRIMARY KEY ,
+    idCronogramaPagos INT AUTO_INCREMENT PRIMARY KEY,
     idCarrera INT,
-	Costo DECIMAL,
+    Costo DECIMAL,
     FOREIGN KEY (idCarrera) REFERENCES Carrera(idCarrera)
 );
 
 -- Tabla SAA
 CREATE TABLE SAA (
-    idSAA INT IDENTITY PRIMARY KEY ,
+    idSAA INT AUTO_INCREMENT PRIMARY KEY,
     idAlumno INT,
     FOREIGN KEY (idAlumno) REFERENCES Alumnos(idAlumno)
 );
 
 -- Tabla PlataformaExterna
 CREATE TABLE PlataformaExterna (
-    idPlataformaExterna INT IDENTITY PRIMARY KEY ,
+    idPlataformaExterna INT AUTO_INCREMENT PRIMARY KEY,
     idAlumno INT,
     FOREIGN KEY (idAlumno) REFERENCES Alumnos(idAlumno)
 );
 
 -- Tabla Notas
 CREATE TABLE Notas (
-    idNotas INT IDENTITY PRIMARY KEY ,
+    idNotas INT AUTO_INCREMENT PRIMARY KEY,
     idMateria INT,
     idAlumno INT,
     nota DECIMAL(4, 2),
@@ -100,16 +101,16 @@ CREATE TABLE Notas (
     FOREIGN KEY (idAlumno) REFERENCES Alumnos(idAlumno)
 );
 
-INSERT INTO Carrera (nombre)VALUES ('Diseño Grafico'),('Ingieneria de Software con IA')
-INSERT INTO Profesor (nombre,correo,contraseña)VALUES ('Walter Coaguila','242424@senati.pe','242424'),('Adrian Gonzales','242425@senati.pe','242425')
-INSERT INTO Alumnos(nombre,correo,contraseña)VALUES ('Zavala Jhosue','141414@senati.pe','141414'),('Angeles Joel','141415@senati.pe','141415'),('Carrillo Juda','141416@senati.pe','141416')
-INSERT INTO Materia(nombre,idCarrera,idProfesor)VALUES('Diseño de packaging',1,1),('Diseño y programación de bases de datos con SQL',2,2),('Desarrollo de Aplicaiones Web',2,2)
-INSERT INTO Horario(idMateria,horaInicio,horaFin)VALUES(1,GETDATE(),GETDATE()),(2,GETDATE(),GETDATE())
-INSERT INTO Aulas(nombre)VALUES('Aula1'),('Aula2')
-INSERT INTO Alumnos_Materias(idAlumno,idMateria)VALUES(1,1),(2,2),(3,3)
-INSERT INTO Laboratorios(nombre)VALUES('Laboratorio1'),('Laboratorio2')
-INSERT INTO Asistencia(idAlumno,idMateria)VALUES(1,1),(2,2),(3,3)
-INSERT INTO CronogramaPagos(idCarrera,Costo)VALUES(1,500),(2,600)
-INSERT INTO SAA(idAlumno)VALUES(1)
-INSERT INTO PlataformaExterna(idAlumno)VALUES(1)
-INSERT INTO Notas(idMateria,idAlumno,nota)VALUES(1,1,20),(2,2,20),(3,3,20)
+-- INSERT INTO Carrera (nombre) VALUES ('Diseño Grafico'),('Ingieneria de Software con IA');
+-- INSERT INTO Materia(nombre,idCarrera,idProfesor)VALUES('Diseño de packaging',1,1),('Diseño y programación de bases de datos con SQL',2,2),('Desarrollo de Aplicaiones Web',2,2);
+-- INSERT INTO Horario(idMateria,horaInicio,horaFin)VALUES(1,GETDATE(),GETDATE()),(2,GETDATE(),GETDATE());
+-- INSERT INTO Aulas(nombre)VALUES('Aula1'),('Aula2');
+-- INSERT INTO Alumnos_Materias(idAlumno,idMateria)VALUES(1,1),(2,2),(3,3);
+-- INSERT INTO Laboratorios(nombre)VALUES('Laboratorio1'),('Laboratorio2');
+-- INSERT INTO Asistencia(idAlumno,idMateria)VALUES(1,1),(2,2),(3,3);
+-- INSERT INTO CronogramaPagos(idCarrera,Costo)VALUES(1,500),(2,600);
+-- INSERT INTO SAA(idAlumno)VALUES(1);
+-- INSERT INTO PlataformaExterna(idAlumno)VALUES(1);
+-- INSERT INTO Notas(idMateria,idAlumno,nota)VALUES(1,1,20),(2,2,20),(3,3,20);
+INSERT INTO Profesor (nombre,correo,contraseña) VALUES ('Walter Coaguila','242424@senati.edu.pe','242424'),('Adrian Gonzales','242425@senati.edu.pe','242425');
+INSERT INTO Alumnos(nombre,correo,contraseña) VALUES ('Zavala Jhosue','141414@senati.pe','141414'),('Angeles Joel','141415@senati.pe','141415'),('Carrillo Juda','141416@senati.pe','141416');
