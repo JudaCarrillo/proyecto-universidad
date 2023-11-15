76fa3566a93796fd1dc31c6bd3e2026ea95518b4
jcarrillo <130939221+JudaCarrillo@users.noreply.github.com>
2023-11-14 23:53:52 -0500

    Mejoras en la base de datos

--- index.php
+++ index.php
@@ -7,28 +7,23 @@
     <title>Inicio</title>
     <!-- css -->
     <link rel="stylesheet" href="./assets/css/style.css">
-
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
+    <img src="./assets/img/logo-senati.png" alt="Logo SENATI">
 
+    <div class="login-container">
+        <p>Acceder</p>
 
-    <div class="form">
         <form action="" method="post">
-            <input type="email" placeholder="Nombre de Usuario">
-            <input type="password" placeholder="Contraseña">
-
+            <input type="email" placeholder="Ingrese su correo" name="email" required>
+            <input type="password" placeholder="Ingrese su contraseña" name="psw" required>
             <button type="submit">Continuar</button>
         </form>
     </div>
-
-
-
-    <!-- bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
 </body>
+
 
 </html>
