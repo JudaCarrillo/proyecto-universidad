166cd5732404b6b8b549314e64fcbf1257038096
Jhosue <150406643+JhosueLeo@users.noreply.github.com>
2023-11-15 01:43:08 -0500

    InterfazAlumno

--- index.php
+++ index.php
@@ -1,40 +1,29 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    
     <!-- css -->
     <link rel="stylesheet" href="./assets/css/style.css">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <!-- bootstrap / js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
 </head>
 
 <body>
-    <div id="loginContainer">
-        <div class="text-center">
-            <img src="./assets/img/logo-senati.png" alt="Logo" id="logo">
-        </div>
-        <form class="login" id="loginForm" action="./controllers/auth/login.php" method="post">
-            <div class="form-group">
-                <input type="email" class="form-control" name="correo" id="correo" placeholder="Correo electrónico" required>
-            </div>
-            <div class="form-group">
-                <input type="password" class="form-control" name="contrasena" id="contrasena" placeholder="Contraseña" required>
-            </div>
-            <button type="button" class="btn btn-primary btn-block" id="loginButton">Iniciar Sesión</button>
+    <img src="./assets/img/logo-senati.png" alt="Logo SENATI">
+    <div class="login-container">
+        <p>Acceder</p>
+
+        <form action="" method="post">
+            <input type="email" placeholder="Ingrese su correo" name="email" required>
+            <input type="password" placeholder="Ingrese su contraseña" name="psw" required>
+            <button type="submit">Continuar</button>
         </form>
     </div>
-
-    <!-- js / jquery -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <!-- js / login -->
-    <script src="./controllers/auth/login.js"></script>
-
 </body>
+
 
 </html>
--- views/student/studentView.php
+++ views/student/studentView.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alumno</title>
-     <!-- css -->
-     <link rel="stylesheet" href="./assets/css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Senati</title>
+<!-- css -->
+<link rel="stylesheet" href="../../assets/css/styleStudent.css">
 </head>
 <body>
-    
-    <div class="img">
-        <img src="../assets/img/ASISTENCIA.png" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
-    </div>
+
+  <header>
+    <img src="../../assets/img/logo-senati" alt="Logo Senati">
+   
+  </header>
+
+  <div class="img">
+    <img src="../../assets/img/ASISTENCIA.png" alt="asistencia">
+    <img src="../../assets/img/HORARIO_CLASE.png" alt="horario">
+    <img src="../../assets/img/REGISTRAR.png" alt="registrar">
+  </div>
+
+
 </body>
 </html>
