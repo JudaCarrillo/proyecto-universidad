325ff5a1450f1b795145fffdc223ccb049759f23
jhoel <150470821+evilzn@users.noreply.github.com>
2023-11-14 23:55:06 -0700

    mejor interfaz

--- index.php
+++ index.php
@@ -1,34 +1,81 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
-    <!-- css -->
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
 
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        #loginContainer {
+            max-width: 250px;
+            width: 100%;
+            padding: 15px;
+            border-radius: 12px;
+            box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.3);
+            background-color: #ffffff;
+        }
+
+        img{
+            width: 190px;
+            
+        }
+        #logo {
+            max-width: 100%;
+            height: auto;
+            margin-bottom: 15px;
+        }
+
+        #loginForm {
+            text-align: center;
+        }
+
+        .form-group {
+            margin-bottom: 15px;
+        }
+
+        #loginButton {
+            background-color: #2600ff;
+            border-color: #2600ff;
+            box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.2);
+        }
+
+        #loginButton:hover {
+            background-color: #2600ff;
+            border-color: #2600ff;
+        }
+    </style>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
-
 <body>
-
-
-    <div class="form">
-        <form action="" method="post">
-            <input type="email" placeholder="Nombre de Usuario">
-            <input type="password" placeholder="Contraseña">
-
-            <button type="submit">Continuar</button>
+    <div id="loginContainer">
+        <div class="text-center">
+            <img src="./assets/img/logo.png" alt="Logo" id="logo">
+        </div>
+        <form id="loginForm">
+            <div class="form-group">
+                <input type="email" class="form-control" id="correo" placeholder="Correo electrónico" required>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="contrasena" placeholder="Contraseña" required>
+            </div>
+            <button type="button" class="btn btn-primary btn-block" id="loginButton" onclick="iniciarSesion()">Iniciar Sesión</button>
         </form>
     </div>
 
-
-
-    <!-- bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
+    <script>
+        function iniciarSesion() {
+            alert("Lógica de inicio de sesión aquí");
+        }
+    </script>
 </body>
-
-</html>
